ca7a746edc10b4d9b24c404572ecc7f71bba942a
ajw027 <ajw027@student.bham.ac.uk>
2024-11-16 17:05:13 +0000

    Add theme toggle button, formalise styling for light theme, add dark theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,36 @@
-import React, {useEffect, useState} from "react";
-import NavBar from './components/NavBar'
-import AllEntries from './routes/AllEntries'
-import NewEntry from './routes/NewEntry'
-import EditEntry from './routes/EditEntry'
-import { EntryProvider } from './utilities/globalContext'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AllEntries from "./routes/AllEntries";
+import EditEntry from "./routes/EditEntry";
+import NewEntry from "./routes/NewEntry";
+import { EntryProvider } from "./utilities/globalContext";
 
 function App() {
-  const [theme , setTheme] = useState("default");
+  const [theme, setTheme] = useState("default");
   function toggleTheme() {
-    if(theme=="default"){
-      setTheme("dark")
-    } else setTheme("default")
+    if (theme == "default") {
+      setTheme("dark");
+    } else setTheme("default");
   }
 
   useEffect(() => {
     document.documentElement.setAttribute("theme", theme);
-  }, [theme])
+  }, [theme]);
 
   return (
     <section className="bg-bg h-screen">
-  <Router>
-    <EntryProvider>
-    <NavBar theme={theme} setTheme={toggleTheme}></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
+      <Router>
+        <EntryProvider>
+          <NavBar theme={theme} setTheme={toggleTheme}></NavBar>
+          <Routes>
+            <Route path="/" element={<AllEntries />}></Route>
+            <Route path="create" element={<NewEntry />}></Route>
+            <Route path="edit/:id" element={<EditEntry />}></Route>
+          </Routes>
+        </EntryProvider>
+      </Router>
     </section>
-    
   );
 }
 export default App;
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,24 @@
-import {NavLink} from 'react-router-dom'
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-export default function NavBar(props:{theme:string; setTheme: React.MouseEventHandler<HTMLButtonElement> | undefined}){
-    return(
-      <nav className="flex justify-center gap-5">
-        <NavLink className="m-3 p-4 text-xl bg-buttons hover:bg-hover rounded-md font-medium text-text" to={'/'}>All Entries</NavLink>
-        <NavLink className="m-3 p-4 text-xl bg-buttons hover:bg-hover rounded-md font-medium text-text" to={'/create'}>New Entry</NavLink>
-        <button className="m-3 p-4 text-xl bg-buttons hover:bg-hover rounded-md font-medium text-text" onClick={props.setTheme}>Toggle theme (current: {props.theme})</button>
-      </nav>
-    )
+export default function NavBar(props: {
+  theme: string;
+  setTheme: React.MouseEventHandler<HTMLButtonElement> | undefined;
+}) {
+  return (
+    <nav className="flex justify-center gap-5">
+      <NavLink className="m-3 p-4 text-xl bg-buttons hover:bg-hover rounded-md font-medium text-text" to={"/"}>
+        All Entries
+      </NavLink>
+      <NavLink className="m-3 p-4 text-xl bg-buttons hover:bg-hover rounded-md font-medium text-text" to={"/create"}>
+        New Entry
+      </NavLink>
+      <button
+        className="m-3 p-4 text-xl bg-buttons hover:bg-hover rounded-md font-medium text-text"
+        onClick={props.setTheme}
+      >
+        Toggle theme (current: {props.theme})
+      </button>
+    </nav>
+  );
 }
